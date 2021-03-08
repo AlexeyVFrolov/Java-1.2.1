4055241622a2bf7909520a40a71cd5125b507f88
AlexeyVFrolov <isbase@mail.ru>
2021-03-08 15:15:02 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int Current_balance = 2_000_000_000;
+        int Transfer_amount = 500_000_000;
+        int Total_amount = Current_balance + Transfer_amount;
+        System.out.println(Total_amount);
+    }
+}
