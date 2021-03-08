53ecd22b6f88707fb9b21ba0f0c616de28396b6a
AlexeyVFrolov <isbase@mail.ru>
2021-03-08 15:56:39 +0300

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         int Current_balance = 2_000_000_000;
         int Transfer_amount = 500_000_000;
         int Total_amount = Current_balance + Transfer_amount;
-        System.out.println(Total_amount);
+        System.out.println(String.format("Итоговое значение счета клиента, рублей: %s", Total_amount));
     }
 }
